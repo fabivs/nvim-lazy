2045702236973763a737e319310ee9786798f76c
Fabio Prizio <fbprz94@gmail.com>
2025-02-26 11:31:36 +0100

    chore: some updates

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,12 +2,12 @@ return {
   "sindrets/diffview.nvim",
   keys = function()
     return {
-      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Git Current Status Diff" },
-      { "<leader>gd", "<cmd>DiffviewFileHistory<cr>", desc = "Git Current Branch Diff" },
+      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Git Current Status Diffview" },
+      { "<leader>gd", "<cmd>DiffviewFileHistory<cr>", desc = "Git Current Branch Diffview" },
       {
         "<leader>gD",
         "<cmd>DiffviewFileHistory %<cr>",
-        desc = "Git Current File Diff",
+        desc = "Git Current File History Diffview",
       },
     }
   end,
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,20 @@
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ""
+    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+      return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+    end
+    return str
+  end
+end
+
 return {
   {
     "b0o/incline.nvim",
@@ -27,6 +44,17 @@ return {
           local icon, color = require("nvim-web-devicons").get_icon_color(filename)
           return { { icon, guifg = color }, { " " }, { filename } }
         end,
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.remove(opts.sections.lualine_b)
+      table.insert(opts.sections.lualine_b, {
+        "branch",
+        fmt = trunc(1000, 12, 80, false),
       })
     end,
   },
