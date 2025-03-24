891c6ec41158276808f0ab331bb449fd52171909
Fabio Prizio <fbprz94@gmail.com>
2025-03-24 01:51:55 +0100

    feat: replace solarized-osaka with gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,6 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
+  "ellisonleao/gruvbox.nvim",
   lazy = true,
   priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  config = true,
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -18,19 +18,10 @@ end
 return {
   {
     "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
     -- Optional: Lazy load Incline
     event = "VeryLazy",
     config = function()
-      local colors = require("solarized-osaka.colors").setup()
-
       require("incline").setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guibg = colors.blue500, guifg = colors.base04 },
-            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-          },
-        },
         window = { margin = { vertical = 0, horizontal = 1 } },
         hide = {
           cursorline = true,
