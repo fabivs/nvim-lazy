835d38941d51b4a7731242288530ec17217ba2fd
Fabio Prizio <fabio.prizio@prima.it>
2025-04-22 11:46:35 +0200

    feat: make colorscheme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
