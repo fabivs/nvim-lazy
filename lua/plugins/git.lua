587b2481d1e66917aceafd9faf82300e1fca839a
Fabio Prizio <fabio.prizio@prima.it>
2025-04-22 11:46:45 +0200

    feat: improve git diffview shortcuts

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,9 +3,9 @@ return {
   keys = function()
     return {
       { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Git Current Status Diffview" },
-      { "<leader>gd", "<cmd>DiffviewFileHistory<cr>", desc = "Git Current Branch Diffview" },
+      { "<leader>gO", "<cmd>DiffviewFileHistory<cr>", desc = "Git Current Branch Diffview" },
       {
-        "<leader>gD",
+        "<leader>gF",
         "<cmd>DiffviewFileHistory %<cr>",
         desc = "Git Current File History Diffview",
       },
