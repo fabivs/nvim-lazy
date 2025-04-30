946db4aa407c14abd9db18271f971bcfbc5ac978
Fabio Prizio <fabio.prizio@prima.it>
2025-04-30 17:54:21 +0200

    feat: small keymap updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,28 +4,22 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Increment/decrement number under cursor
-keymap.set("n", "+", "<C-a>", opts)
-keymap.set("n", "-", "<C-x>", opts)
-
--- Select all in normal mode
-keymap.set("n", "<C-a>", "gg<S-v>G", opts)
-
 -- Clipboard
 -- paste and delete without updating the clipboard
-vim.keymap.set("x", "<BS>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<BS>d", [["_d]])
-vim.keymap.set({ "n", "v" }, "<BS>D", [["_D]])
+keymap.set("x", "<BS>p", [["_dP]])
+keymap.set({ "n", "v" }, "<BS>d", [["_d]])
+keymap.set({ "n", "v" }, "<BS>D", [["_D]])
 -- copy to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]], { remap = true })
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]], { remap = true })
 
-vim.keymap.set("n", "yc", "yygccp", { remap = true, desc = "Comment and duplicate current line" })
+keymap.set("n", "yc", "yygccp", { remap = true, desc = "Comment and duplicate current line" })
 
 -- Neo-tree vinegar keybind
 -- keymap.set("n", "-", ":Neotree toggle current reveal_force_cwd<cr>", opts)
 keymap.set("n", "-", ":Explore<CR>", opts)
 
+-- Copy relative path
 keymap.set("n", "<leader>fp", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy relative Path" })
 keymap.set(
   "n",
