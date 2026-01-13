69d7d8b09956cfddab4ac8843a3e421e3694661d
Fabio Prizio <fabio.prizio@prima.it>
2026-01-13 12:10:13 +0100

    chore: messing around with colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  -- add colorscheme
-  { "vague2k/vague.nvim" },
-
-  -- Configure LazyVim to load the colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vague",
-    },
-  },
+  -- -- add colorscheme
+  -- { "vague2k/vague.nvim" },
+  --
+  -- -- Configure LazyVim to load the colorscheme
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "vague",
+  --   },
+  -- },
 }
