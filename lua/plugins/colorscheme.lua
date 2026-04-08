2945c34df4d74caf5d10472f20da619451eba585
Fabio Prizio <fabio.prizio@prima.it>
2026-04-08 18:04:15 +0200

    chore: restore default colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,1 @@
-return {
-  -- -- add colorscheme
-  -- { "vague2k/vague.nvim" },
-  --
-  -- -- Configure LazyVim to load the colorscheme
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "vague",
-  --   },
-  -- },
-}
+return {}
