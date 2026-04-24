d2f47daed87a8028f333d6b21eb0eb0ffa2b1c86
Fabio Prizio <fabio.prizio@prima.it>
2026-04-24 12:30:49 +0200

    feat: switch to a maintained Diffview plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,9 @@
 return {
-  "sindrets/diffview.nvim",
+  "dlyongemallo/diffview.nvim",
   keys = function()
     return {
       { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Git Current Status Diffview" },
+      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Close Diffview (if open)" },
       { "<leader>gO", "<cmd>DiffviewFileHistory<cr>", desc = "Git Current Branch Diffview" },
       {
         "<leader>gF",
