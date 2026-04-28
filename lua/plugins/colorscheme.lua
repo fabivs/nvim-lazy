a166e0a5a6d33766183b5ed6c283fd7538839e4b
Fabio Prizio <fabio.prizio@prima.it>
2026-04-28 17:42:46 +0200

    feat: kanagawa color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  -- add colorscheme
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
