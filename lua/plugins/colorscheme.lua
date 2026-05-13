5c33bba8b7b3ed6e4e2355f8498121d66398e1cb
Fabio Prizio <fabio.prizio@prima.it>
2026-05-13 17:13:48 +0200

    Revert "feat: kanagawa color scheme"
    This reverts commit a166e0a5a6d33766183b5ed6c283fd7538839e4b.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,1 @@
-return {
-  -- add colorscheme
-  { "rebelot/kanagawa.nvim" },
-
-  -- Configure LazyVim to load the colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
-  },
-}
+return {}
